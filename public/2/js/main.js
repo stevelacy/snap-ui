d287f93e940008841212466d08704ddc84aabf9c
Steve Lacy <me@slacy.me>
2014-07-25 11:09:31 -0700

    fix click event

--- public/2/js/main.js
+++ public/2/js/main.js
@@ -1,15 +1,16 @@
 (function() {
   $(document).ready(function() {
     $(".action-menu .menu").hide();
-    $(".action-menu .button").click(function() {
-      return $(this).find(".menu").fadeIn();
-    });
-    return $(document).mouseout(function(e) {
+    $(document).mouseout(function(e) {
       var container;
       container = $(".action-menu .menu");
       if (!container.is(e.target && container.has(e.target).length === 0)) {
         return container.fadeOut();
       }
+    });
+    return $(".action-menu .button").click(function() {
+      console.log($(this).parent().find(".menu"));
+      return $(this).parent().find(".menu").fadeToggle();
     });
   });
 
--- src/2/js/main.coffee
+++ src/2/js/main.coffee
@@ -5,11 +5,12 @@ $(document).ready ->
 
   # click functions
 
-  $(".action-menu .button").click ->
-    $(@).find(".menu").fadeIn()
-
-
   $(document).mouseout (e) ->
     container = $ ".action-menu .menu"
     if !container.is e.target and container.has(e.target).length is 0
       container.fadeOut()
+
+  $(".action-menu .button").click ->
+    console.log $(@).parent().find(".menu")
+    $(@).parent().find(".menu").fadeToggle()
+
